145e65476804319d884210e125fa0631b9e9867e
vaibhaVishwakarma <vaibhavofficial709@gmail.com>
2026-06-22 00:40:24 +0530

    commit ~6,2-~4,2-~2,2 #~8,2

--- update.bat
+++ update.bat
@@ -1,5 +1,19 @@
 git add .
-git commit -m "update $(date +%Y-%m-%d)"
+@echo off
+:: Fetch a reliable, region-independent timestamp
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
+
+:: Extract date and hour components
+set year=%datetime:~2,2%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+set hour=%datetime:~8,2%
+
+:: Build the commit message string
+set msg=commit %day%-%month%-%year% #%hour%
+
+:: Run the Git commands
+git commit -m "%msg%"
 git push origin main
 
 echo "\n\nhttps://vaibhavishwakarma.github.io/checklist/"
